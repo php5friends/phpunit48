a19032f6946967fb4857bc801602243fabd2d6bf
USAMI Kenta <tadsan@zonu.me>
2017-06-19 02:18:24 +0900

    Add .phan config

---
+++ .phan/config.php
@@ -0,0 +1,291 @@
+<?php
+
+use \Phan\Issue;
+
+/**
+ * This configuration will be read and overlayed on top of the
+ * default configuration. Command line arguments will be applied
+ * after this file is read.
+ *
+ * @see src/Phan/Config.php
+ * See Config for all configurable options.
+ *
+ * A Note About Paths
+ * ==================
+ *
+ * Files referenced from this file should be defined as
+ *
+ * ```
+ *   Config::projectPath('relative_path/to/file')
+ * ```
+ *
+ * where the relative path is relative to the root of the
+ * project which is defined as either the working directory
+ * of the phan executable or a path passed in via the CLI
+ * '-d' flag.
+ */
+return [
+
+    // If true, missing properties will be created when
+    // they are first seen. If false, we'll report an
+    // error message.
+    "allow_missing_properties" => false,
+
+    // Allow null to be cast as any type and for any
+    // type to be cast to null.
+    "null_casts_as_any_type" => false,
+
+    // If enabled, scalars (int, float, bool, string, null)
+    // are treated as if they can cast to each other.
+    'scalar_implicit_cast' => false,
+
+    // If true, seemingly undeclared variables in the global
+    // scope will be ignored. This is useful for projects
+    // with complicated cross-file globals that you have no
+    // hope of fixing.
+    'ignore_undeclared_variables_in_global_scope' => false,
+
+    // Backwards Compatibility Checking
+    'backward_compatibility_checks' => false,
+
+    // If true, check to make sure the return type declared
+    // in the doc-block (if any) matches the return type
+    // declared in the method signature. This process is
+    // slow.
+    'check_docblock_signature_return_type_match' => true,
+
+    // If true, check to make sure the return type declared
+    // in the doc-block (if any) matches the return type
+    // declared in the method signature. This process is
+    // slow.
+    'check_docblock_signature_param_type_match' => true,
+
+    // (*Requires check_docblock_signature_param_type_match to be true*)
+    // If true, make narrowed types from phpdoc override
+    // the real types from the signature, when real types exist.
+    // Ignore incompatible or wider phpdoc union types.
+    // (E.g. allows specifying desired lists of subclasses,
+    //  or to indicate a preference for non-nullable types over nullable types)
+    // Affects analysis of the body of the method and the param types passed in by callers.
+    'prefer_narrowed_phpdoc_param_types' => true,
+
+    // If enabled, check all methods that override a
+    // parent method to make sure its signature is
+    // compatible with the parent's. This check
+    // can add quite a bit of time to the analysis.
+    'analyze_signature_compatibility' => true,
+
+    // Set to true in order to attempt to detect dead
+    // (unreferenced) code. Keep in mind that the
+    // results will only be a guess given that classes,
+    // properties, constants and methods can be referenced
+    // as variables (like `$class->$property` or
+    // `$class->$method()`) in ways that we're unable
+    // to make sense of.
+    'dead_code_detection' => false,
+
+    // Run a quick version of checks that takes less
+    // time
+    "quick_mode" => false,
+
+    // If true, then try to simplify AST into a form which improves Phan's type inference.
+    // E.g. rewrites `if (!is_string($foo)) { return; } b($foo);`
+    // into `if (is_string($foo)) {b($foo);} else {return;}`
+    // This may conflict with 'dead_code_detection'
+    // This slows down analysis noticeably.
+    "simplify_ast" => false,
+
+    // Enable or disable support for generic templated
+    // class types.
+    'generic_types_enabled' => true,
+
+    // Setting this to true makes the process assignment for file analysis
+    // as predictable as possible, using consistent hashing.
+    // Even if files are added or removed, or process counts change,
+    // relatively few files will move to a different group.
+    // (use when the number of files is much larger than the process count)
+    // NOTE: If you rely on Phan parsing files/directories in the order
+    // that they were provided in this config, don't use this)
+    // See https://github.com/etsy/phan/wiki/Different-Issue-Sets-On-Different-Numbers-of-CPUs
+    'consistent_hashing_file_order' => false,
+
+    // Override to hardcode existence and types of (non-builtin) globals.
+    // Class names must be prefixed with '\\'.
+    'globals_type_map' => [],
+
+    // The minimum severity level to report on. This can be
+    // set to Issue::SEVERITY_LOW, Issue::SEVERITY_NORMAL or
+    // Issue::SEVERITY_CRITICAL.
+    'minimum_severity' => Issue::SEVERITY_LOW,
+
+    // Add any issue types (such as 'PhanUndeclaredMethod')
+    // here to inhibit them from being reported
+    'suppress_issue_types' => [
+        // 'PhanUndeclaredMethod',
+    ],
+
+    // If empty, no filter against issues types will be applied.
+    // If non-empty, only issues within the list will be emitted
+    // by Phan.
+    'whitelist_issue_types' => [
+        // 'PhanAccessMethodPrivate',
+        // 'PhanAccessMethodProtected',
+        // 'PhanAccessNonStaticToStatic',
+        // 'PhanAccessPropertyPrivate',
+        // 'PhanAccessPropertyProtected',
+        // 'PhanAccessSignatureMismatch',
+        // 'PhanAccessSignatureMismatchInternal',
+        // 'PhanAccessStaticToNonStatic',
+        // 'PhanCompatibleExpressionPHP7',
+        // 'PhanCompatiblePHP7',
+        // 'PhanContextNotObject',
+        // 'PhanDeprecatedClass',
+        // 'PhanDeprecatedFunction',
+        // 'PhanDeprecatedProperty',
+        // 'PhanEmptyFile',
+        // 'PhanNonClassMethodCall',
+        // 'PhanNoopArray',
+        // 'PhanNoopClosure',
+        // 'PhanNoopConstant',
+        // 'PhanNoopProperty',
+        // 'PhanNoopVariable',
+        // 'PhanParamRedefined',
+        // 'PhanParamReqAfterOpt',
+        // 'PhanParamSignatureMismatch',
+        // 'PhanParamSignatureMismatchInternal',
+        // 'PhanParamSpecial1',
+        // 'PhanParamSpecial2',
+        // 'PhanParamSpecial3',
+        // 'PhanParamSpecial4',
+        // 'PhanParamTooFew',
+        // 'PhanParamTooFewInternal',
+        // 'PhanParamTooMany',
+        // 'PhanParamTooManyInternal',
+        // 'PhanParamTypeMismatch',
+        // 'PhanParentlessClass',
+        // 'PhanRedefineClass',
+        // 'PhanRedefineClassInternal',
+        // 'PhanRedefineFunction',
+        // 'PhanRedefineFunctionInternal',
+        // 'PhanStaticCallToNonStatic',
+        // 'PhanSyntaxError',
+        // 'PhanTraitParentReference',
+        // 'PhanTypeArrayOperator',
+        // 'PhanTypeArraySuspicious',
+        // 'PhanTypeComparisonFromArray',
+        // 'PhanTypeComparisonToArray',
+        // 'PhanTypeConversionFromArray',
+        // 'PhanTypeInstantiateAbstract',
+        // 'PhanTypeInstantiateInterface',
+        // 'PhanTypeInvalidLeftOperand',
+        // 'PhanTypeInvalidRightOperand',
+        // 'PhanTypeMismatchArgument',
+        // 'PhanTypeMismatchArgumentInternal',
+        // 'PhanTypeMismatchDefault',
+        // 'PhanTypeMismatchForeach',
+        // 'PhanTypeMismatchProperty',
+        // 'PhanTypeMismatchReturn',
+        // 'PhanTypeMissingReturn',
+        // 'PhanTypeNonVarPassByRef',
+        // 'PhanTypeParentConstructorCalled',
+        // 'PhanTypeVoidAssignment',
+        // 'PhanUnanalyzable',
+        // 'PhanUndeclaredClass',
+        // 'PhanUndeclaredClassCatch',
+        // 'PhanUndeclaredClassConstant',
+        // 'PhanUndeclaredClassInstanceof',
+        // 'PhanUndeclaredClassMethod',
+        // 'PhanUndeclaredClassReference',
+        // 'PhanUndeclaredConstant',
+        // 'PhanUndeclaredExtendedClass',
+        // 'PhanUndeclaredFunction',
+        // 'PhanUndeclaredInterface',
+        // 'PhanUndeclaredMethod',
+        // 'PhanUndeclaredProperty',
+        // 'PhanUndeclaredStaticMethod',
+        // 'PhanUndeclaredStaticProperty',
+        // 'PhanUndeclaredTrait',
+        // 'PhanUndeclaredTypeParameter',
+        // 'PhanUndeclaredTypeProperty',
+        // 'PhanUndeclaredVariable',
+        // 'PhanUnreferencedClass',
+        // 'PhanUnreferencedConstant',
+        // 'PhanUnreferencedMethod',
+        // 'PhanUnreferencedProperty',
+        // 'PhanVariableUseClause',
+    ],
+
+    // A list of files to include in analysis
+    'file_list' => [
+    ],
+
+    // A regular expression to match files to be excluded
+    // from parsing and analysis and will not be read at all.
+    //
+    // This is useful for excluding groups of test or example
+    // directories/files, unanalyzable files, or files that
+    // can't be removed for whatever reason.
+    // (e.g. '@Test\.php$@', or '@vendor/.*/(tests|Tests)/@')
+    'exclude_file_regex' => '@^vendor/.*/(tests|Tests)/@',
+
+    // A file list that defines files that will be excluded
+    // from parsing and analysis and will not be read at all.
+    //
+    // This is useful for excluding hopelessly unanalyzable
+    // files that can't be removed for whatever reason.
+    'exclude_file_list' => [],
+
+    // The number of processes to fork off during the analysis
+    // phase.
+    'processes' => 1,
+
+    // A list of directories that should be parsed for class and
+    // method information. After excluding the directories
+    // defined in exclude_analysis_directory_list, the remaining
+    // files will be statically analyzed for errors.
+    //
+    // Thus, both first-party and third-party code being used by
+    // your application should be included in this list.
+    'directory_list' => [
+        'src/',
+        'tests/',
+        'vendor/phpspec/prophecy/src/Prophecy/',
+        'vendor/phpunit/php-code-coverage/src/',
+        'vendor/phpunit/php-file-iterator/src/',
+        'vendor/phpunit/php-text-template/src/',
+        'vendor/phpunit/php-timer/src/',
+        'vendor/phpunit/php-token-stream/src/',
+        'vendor/phpunit/phpunit-mock-objects/src/Framework/MockObject/',
+        'vendor/sebastian/comparator/src/',
+        'vendor/sebastian/diff/src/',
+        'vendor/sebastian/environment/src/',
+        'vendor/sebastian/exporter/src/',
+        'vendor/sebastian/global-state/src/',
+        'vendor/sebastian/recursion-context/src/',
+        'vendor/sebastian/version/src/',
+    ],
+
+    // List of case-insensitive file extensions supported by Phan.
+    // (e.g. php, html, htm)
+    'analyzed_file_extensions' => ['php'],
+
+    // A directory list that defines files that will be excluded
+    // from static analysis, but whose class and method
+    // information should be included.
+    //
+    // Generally, you'll want to include the directories for
+    // third-party code (such as "vendor/") in this list.
+    //
+    // n.b.: If you'd like to parse but not analyze 3rd
+    //       party code, directories containing that code
+    //       should be added to the `directory_list` as
+    //       to `exclude_analysis_directory_list`.
+    "exclude_analysis_directory_list" => [
+        'vendor/'
+    ],
+
+    // A list of plugin files to execute
+    'plugins' => [
+    ],
+
+];
